ac3f4929ac3e9eece1b30d9fc6799dd67af2152e
Arbaaz <khanarbaj45@gmail.com>
2020-12-01 21:21:21 +0530

    Feat : Display Rectangle With 2 Color

--- Rectangle with 2 colors.py
+++ Rectangle with 2 colors.py
@@ -19,3 +19,25 @@ while True:
 
     # Detect the faces
     faces = body_cascade.detectMultiScale(gray_img, 1.1, 1)
+
+    # display rectrangle
+    i = 0
+    for (x, y, w, h) in faces:
+        if i % 2 == 0:
+            cv2.rectangle(color_img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            i += 1
+        else:
+            cv2.rectangle(color_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            i += 1
+
+            # display image
+        cv2.imshow('img', color_img)
+
+        # v_writer.write(color_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
